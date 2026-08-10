5414d0dd0b9c38946f7f47c737ace21c2f11c4a9
dragonx943 <premieregirl26@gmail.com>
2026-08-10 23:21:18 +0700

    Update dex, tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-﻿import tasks.ReportGenerateTask
+import tasks.ReportGenerateTask
+import tasks.DexPluginTask
 
 plugins {
     `java-library`
@@ -20,8 +21,7 @@ ksp {
 }
 
 tasks.jar {
-    // remember to dex it with d8 before sideloading
-	archiveFileName.set("gekkoushi.jar")
+	archiveFileName.set("raw.jar")
 	exclude("android/**")
 	exclude("androidx/annotation/**")
 	exclude("androidx/preference/**")
@@ -72,6 +72,19 @@ dependencies {
     testRuntimeOnly(libs.junit.launcher)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.quickjs)
+}
+
+tasks.register("buildJar") {
+    description = "Build all sources to a JAR file"
+    dependsOn("dexJar")
+}
+
+tasks.register<DexPluginTask>("dexJar") {
+    description = "Dex classes after build"
+    dependsOn(tasks.jar)
+    inputJar.set(tasks.jar.flatMap { it.archiveFile })
+    outputJar.set(layout.projectDirectory.file("build/libs/gekkoushi.jar"))
+    classpath.from(configurations.runtimeClasspath)
 }
 
 tasks.register<ReportGenerateTask>("generateTestsReport") {
